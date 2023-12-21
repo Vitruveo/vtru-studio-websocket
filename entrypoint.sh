32df9cedb462b813a4e94d732f40ec2cae6f385f
Matheus Paice <matheus.paice@jbtec.com.br>
2023-12-21 14:36:28 -0300

    feat: add config deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 if [ "x$@" = "xwait" ] ; then
-    while true ; do
-        date
-        node index.js
-        sleep 300
-    done
+    if [ "x$RABBITMQ_PORT" = "x" ] ; then
+        export RABBITMQ_PORT=5672
+    fi
+    if [ "x$RABBITMQ_HOST" = "x" ] ; then
+        echo RABBITMQ_HOST não definido
+    else
+        node tools/wait.js $RABBITMQ_HOST $RABBITMQ_PORT
+    fi
+    if [ "x$NODE_ENV" = "xproduction" ] ; then
+        while true ; do
+            date
+            npm start
+            sleep 300
+        done
+    else
+        echo NODE_ENV já definido: $NODE_ENV
+        npm run $NODE_ENV
+    fi
 else
     echo Executando comando: $@
     $@
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-export const TOKEN_ADMINS = process.env.ADMIN || 'admin';
-export const TOKEN_CREATORS = process.env.CREATORS || 'creator';
+export const TOKEN_ADMINS = process.env.TOKEN_ADMINS || 'admin';
+export const TOKEN_CREATORS = process.env.TOKEN_CREATORS || 'creator';
 
 export * from './rabbitmq';
 export * from './sentry';
